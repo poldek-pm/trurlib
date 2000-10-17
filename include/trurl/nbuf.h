07c7e3eb9c1bac9913605d61b992f9beb359c76d
Paweł Gajda <mis@pld-linux.org>
2000-10-17 17:38:51 +0000

    - nbuf redesign

--- include/trurl/nbuf.h
+++ include/trurl/nbuf.h
@@ -7,22 +7,37 @@
 #ifndef TRURL_BUF_H
 #define TRURL_BUF_H
 
+#define TN_BUF_CONSTSIZE         (1 << 0)
+#define TN_BUF_CONSTDATA         (1 << 1)
+
 typedef struct trurl_buf tn_buf;
 
 tn_buf *n_buf_new(int size);
+tn_buf *n_buf_init(tn_buf *buf, void *buffer, int size);
 tn_buf *n_buf_clean(tn_buf *buf);
-
 void n_buf_free(tn_buf *buf);
 
-void *n_buf_add(tn_buf *buf, const void *data, int size);
+tn_buf *n_buf_ctl(tn_buf *buf, unsigned flags);
 
-char *n_buf_addstring(tn_buf *buf, const char *str, int with_zero);
+int n_buf_add(tn_buf *buf, const void *data, int size);
 
+int n_buf_addstring(tn_buf *buf, const char *str, int with_zero);
 #define n_buf_addstr(buf, str)  n_buf_addstring(buf, str, 0)
 #define n_buf_addstrz(buf, str) n_buf_addstring(buf, str, 1)
 
 int n_buf_size(const tn_buf *buf);
 void *n_buf_ptr(const tn_buf *buf); /* returns buffer data pointer */
+
+
+struct trurl_buf_iterator {
+    tn_buf *nbuf;
+    size_t  offs;
+};
+
+typedef struct trurl_buf_iterator tn_buf_it;
+
+void n_buf_it_init(tn_buf_it *bufi, tn_buf *buf);
+void *n_buf_it_get(tn_buf_it *bufi, size_t size);
 
 #endif /* TRURL_BUF_H */
     
--- nbuf.c
+++ nbuf.c
@@ -45,7 +45,7 @@ struct trurl_buf {
     unsigned char *data;
     size_t        allocated;
     size_t        size;
-    size_t        initial_size;
+    unsigned int  flags;
 };
 
 
@@ -57,36 +57,58 @@ tn_buf *n_buf_new(int initial_size)
 	return NULL;
 
     n_assert(initial_size >= 0);
+    
+    buf->allocated = initial_size;
+    buf->size = 0;
 
-    if (initial_size < 1)
-	initial_size = 2;
-
-    if ((buf->data = malloc(initial_size)) == NULL) {
-	free(buf);
-	buf = NULL;
+    if (initial_size == 0) {
+        buf->data = NULL;
+        
+    } else if ((buf->data = malloc(initial_size)) != NULL) {
+        buf->data[0] = '\0';
 
     } else {
-        buf->data[0] = '\0';
-        buf->allocated = initial_size;
-        buf->size = 0;
-        buf->initial_size = initial_size;
+        free(buf);
+        buf = NULL;
     }
+    
+    return buf;
+}
 
+
+tn_buf *n_buf_init(tn_buf *buf, void *buffer, int size)
+{
+    n_assert(buf->data = NULL);
+    
+    buf->data = buffer;
+    buf->allocated = size;
+    buf->size = size;
+    buf->flags = TN_BUF_CONSTSIZE | TN_BUF_CONSTDATA;
+    return buf;
+}
+
+
+tn_buf *n_buf_ctl(tn_buf *buf, unsigned flags) 
+{
+    buf->flags |= flags;
     return buf;
 }
 
 
 tn_buf *n_buf_clean(tn_buf *buf)
 {
-    buf->size = 0;
-    buf->data[0] = '\0';
+    if (!(buf->flags & TN_BUF_CONSTDATA)) {
+        buf->size = 0;
+        buf->data[0] = '\0';
+    }
     return buf;
 }
 
 
 void n_buf_free(tn_buf *buf)
 {
-    free(buf->data);
+    if (!(buf->flags & TN_BUF_CONSTDATA)) 
+        free(buf->data);
     buf->data = NULL;
     free(buf);
 }
@@ -97,8 +119,13 @@ static tn_buf *n_buf_realloc(tn_buf *buf, size_t new_size)
     register int diff;
 
     diff = new_size - buf->allocated;
-
+    
     n_assert(diff > 0);
+
+    if (buf->flags & TN_BUF_CONSTSIZE) {
+        trurl_die("n_buf_grow: grow request for const size buffer");
+        return NULL;
+    }
 
     if (diff > 0) {
 	void *tmp;
@@ -120,7 +147,7 @@ static tn_buf *n_buf_grow(tn_buf *buf, size_t req_size)
     register size_t new_size = buf->allocated;
 
     while (req_size >= new_size) 
-        new_size += buf->initial_size;
+        new_size *= 2;
     
     return n_buf_realloc(buf, new_size);
 }
@@ -138,34 +165,49 @@ void *n_buf_dptr(const tn_buf *buf)
 }
 
 
-void *n_buf_add(tn_buf *buf, const void *data, int size)
+int n_buf_add(tn_buf *buf, const void *data, int size)
 {
-    register void *p;
-
+    int offs;
+    
     if (buf->allocated - buf->size < (unsigned)size) 
         if (n_buf_grow(buf, buf->allocated + size) == NULL)
-	    return NULL;
-    
-    p = &buf->data[buf->size];
-    memcpy(p, data, size);
+	    return -1;
+
+    memcpy(&buf->data[buf->size], data, size);
+    offs = buf->size;
     buf->size += size;
 
-    return p;
+    return offs;
 }
 
 
-char *n_buf_addstring(tn_buf *buf, const char *str, int with_zero)
+int n_buf_addstring(tn_buf *buf, const char *str, int with_zero)
 {
-    register void *p;
     register int len;
 
     len = strlen(str);
-
+    
     if (with_zero)
         len++;
-
-    p = n_buf_add(buf, str, len);
     
-    return p;
+    return n_buf_add(buf, str, len);
+}
+
+void n_buf_it_init(tn_buf_it *bufi, tn_buf *buf) 
+{
+    bufi->nbuf = buf;
+    bufi->offs = 0;
+}
+
+void *n_buf_it_get(tn_buf_it *bufi, size_t size) 
+{
+    unsigned char *ptr;
+    
+    if (bufi->offs + size > bufi->nbuf->size)
+        return NULL;
+
+    ptr = &bufi->nbuf->data[bufi->offs];
+    bufi->offs += size;
+    return ptr;
 }
 
