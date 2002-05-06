fd101a097c6c563eba4ecdc23a5d97a91ccc241e
Paweł Gajda <mis@pld-linux.org>
2002-05-06 17:01:04 +0000

    - fixed static link: moved xmalloc*()s to separate module

--- nmalloc.c
+++ nmalloc.c
@@ -50,7 +50,7 @@ static void nomem(void)
     if (mem_fail_fn != NULL)
 	mem_fail_fn();
     else {
-        printf("Memory exhausted");
+        printf("Memory exhausted\n");
         exit(1);
     }
 }
@@ -114,34 +114,5 @@ void n_free(void *ptr)
 {
     n_assert(ptr != NULL);
     free(ptr);
-}
-
-
-
-/* be compatibile with < 0.43.7 */
-
-void *xmalloc(size_t size)
-{
-    return n_malloc(size);
-}
-
-void *xcalloc(size_t nmemb, size_t size)
-{
-    return n_calloc(nmemb, size);
-}
-
-void *xrealloc(void *ptr, size_t size)
-{
-    return n_realloc(ptr, size);
-}
-
-char *xstrdup(const char *s) 
-{
-    return n_strdup(s);
-}
-
-void xfree(void *ptr) 
-{
-    n_free(ptr);
 }
 
---
+++ xmalloc.c
@@ -0,0 +1,28 @@
+#include "nmalloc.h"
+/* be compatibile with < 0.43.7 */
+
+void *xmalloc(size_t size)
+{
+    return n_malloc(size);
+}
+
+void *xcalloc(size_t nmemb, size_t size)
+{
+    return n_calloc(nmemb, size);
+}
+
+void *xrealloc(void *ptr, size_t size)
+{
+    return n_realloc(ptr, size);
+}
+
+char *xstrdup(const char *s) 
+{
+    return n_strdup(s);
+}
+
+void xfree(void *ptr) 
+{
+    n_free(ptr);
+}
+
