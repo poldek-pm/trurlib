91447e85e9e532fe7ed826bdb713b020b2a6c534
Paweł Gajda <mis@pld-linux.org>
2003-04-13 22:11:38 +0000

    + n_strdupap()

--- include/trurl/nmalloc.h
+++ include/trurl/nmalloc.h
@@ -20,4 +20,14 @@ void n_cfree(void *ptr);
 char *n_strdupl(const char *s, size_t length);
 void *n_memdup(const void *ptr, size_t size);
 
+/* n_strdupap(src, &dest) */
+#define n_strdupap(s, dp)                     \
+   do {	                                      \
+      const char *ss = (s);                   \
+      char  **dptr = (dp);                    \
+      int len = strlen(ss) + 1;               \
+      *dptr = alloca(len);	                  \
+      memcpy(*dptr, ss, len);				  \
+   } while (0);
+
 #endif /* NMALLOC_H */
