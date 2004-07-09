5e0869fcd40fc48449e0cd7c4c18fbbbaeef769e
Paweł Gajda <mis@pld-linux.org>
2004-07-09 17:24:46 +0000

    - dump()

--- n_hash_map.c
+++ n_hash_map.c
@@ -19,3 +19,21 @@ int n_hash_map(const tn_hash *ht, void (*map_fn) (const char *, void *))
 
     return n;
 }
+
+int n_hash_dump(const tn_hash *ht)
+{
+    register size_t i, n = 0;
+    register struct hash_bucket *tmp;
+
+    for (i = 0; i < ht->size; i++) {
+        if (ht->table[i] == NULL)
+            continue;
+        
+        for (tmp = ht->table[i]; tmp != NULL; tmp = tmp->next) {
+            printf(" %s: %p\n", tmp->key, tmp->data);
+            n++;
+        }
+    }
+
+    return n;
+}
